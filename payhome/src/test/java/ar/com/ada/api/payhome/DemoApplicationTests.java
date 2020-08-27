49c2e9b9a61429b4a93cd2a53aea1ffafe3bf03c
JuanaSF <juana_29_1@hotmail.com>
2020-08-26 23:19:29 -0300

    Agrego test unitarios para verificar que la validaciones hechas para la verificar el id impositivo, y el nombre funcionen correctamente. Se hicieron UnitTest que validan los datos correctos y los datos incorrectos. En este caso solo valido los datos para Empresa, creando una empresa ficticia en el momento. Me queda pendiente hacer lo mismo para un Deudor. Se agrega los settings de la base de datos H2 en appliction.properties.

--- payhome/src/test/java/ar/com/ada/api/payhome/DemoApplicationTests.java
+++ payhome/src/test/java/ar/com/ada/api/payhome/DemoApplicationTests.java
@@ -1,13 +1,88 @@
 package ar.com.ada.api.payhome;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ar.com.ada.api.payhome.entities.*;
+import ar.com.ada.api.payhome.entities.Pais.TipoIdImpositivoEnum;
+import ar.com.ada.api.payhome.services.*;
+import ar.com.ada.api.payhome.utils.Validador.ResultadoValidacionEnum;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	EmpresaService empresaService;
+
+	/* Test para Empresa
+	Test1: que valide todos los datos correctamente
+	Test2: que valide el Id Impositivo con valores incorrectos
+	test3: que valide el nombre con valores incorrectos
+	*/
+
 	@Test
-	void contextLoads() {
+	void empresa_IdImpositivo_correcto() {
+
+		ResultadoValidacionEnum resultado;
+
+		Empresa empresa = new Empresa();
+        empresa.setPaisId(32);
+        empresa.setTipoIdImpositivo(TipoIdImpositivoEnum.CUIT);
+        empresa.setIdImpositivo("5555432210090");
+		empresa.setNombre("Zaraza Testing SA");
+		
+		resultado = empresaService.validarDatosEmpresa(empresa.getNombre(), empresa.getIdImpositivo());
+
+		assertTrue(resultado == ResultadoValidacionEnum.OK);
+	}
+
+	@Test
+	void empresa_IdImpositivo_incorrecto() {
+
+		ResultadoValidacionEnum resultado;
+
+		Empresa empresa = new Empresa();
+        empresa.setPaisId(32);
+        empresa.setTipoIdImpositivo(TipoIdImpositivoEnum.CUIT);
+        empresa.setIdImpositivo("5555432210090X"); // <--Agrego una letra para que sea invalido
+		empresa.setNombre("Zaraza Testing SA");
+		
+		resultado = empresaService.validarDatosEmpresa(empresa.getNombre(), empresa.getIdImpositivo());
+		assertTrue(resultado == ResultadoValidacionEnum.ID_IMPOSITIVO_INVALIDO);
+
+		empresa.setIdImpositivo("12345"); // tiene menos numeros que el minimo permitido
+		resultado = empresaService.validarDatosEmpresa(empresa.getNombre(), empresa.getIdImpositivo());
+		assertTrue(resultado == ResultadoValidacionEnum.ID_IMPOSITIVO_INVALIDO);
+
+		empresa.setIdImpositivo("12345000000433844747657112"); // supera el maximo permitido
+		resultado = empresaService.validarDatosEmpresa(empresa.getNombre(), empresa.getIdImpositivo());
+		assertTrue(resultado == ResultadoValidacionEnum.ID_IMPOSITIVO_INVALIDO);
+
+		empresa.setIdImpositivo(null); // el id es nulo
+		resultado = empresaService.validarDatosEmpresa(empresa.getNombre(), empresa.getIdImpositivo());
+		assertTrue(resultado == ResultadoValidacionEnum.ID_IMPOSITIVO_INVALIDO);
+	}
+
+	@Test
+	void empresa_nombre_incorrecto() {
+
+		ResultadoValidacionEnum resultado;
+
+		Empresa empresa = new Empresa();
+        empresa.setPaisId(32);
+        empresa.setTipoIdImpositivo(TipoIdImpositivoEnum.CUIT);
+        empresa.setIdImpositivo("5555432210090");
+		empresa.setNombre(null); // el nombre es nulo
+		
+		resultado = empresaService.validarDatosEmpresa(empresa.getNombre(), empresa.getIdImpositivo());
+		assertTrue(resultado == ResultadoValidacionEnum.NOMBRE_INVALIDO);
+
+		//el nombre supera el maximo permitido
+		empresa.setNombre("eeeeeeeeeeeeeeeeeeeeelNooooooooooooooooooooooooombreeeeeeeeeeeeeeeeeeeeeeeeeeeeeMaaaaaaaaaaaaaaaasLaaaaaaargoooooooo");
+		assertTrue(resultado == ResultadoValidacionEnum.NOMBRE_INVALIDO);
 	}
 
 }
